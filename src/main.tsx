f9079aeaa88bf36b611063238b191fa53ce8e871
Malia Wakesho-Ajwang <maz.obiri@emory.edu>
2025-08-19 21:02:23 +0200

    moved to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/global.css'
 import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import PomodoroPage from './pages/PomodoroPage.tsx'
 import { PomodoroSettingsProvider } from './context/PomodoroSettingsContext';
 import { PomodoroProvider } from './context/PomodoroContext.tsx'
@@ -15,7 +15,7 @@ import TodoPage from './pages/TodoPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/pomodoro" element={
@@ -30,6 +30,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/todo" element={<TodoPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
